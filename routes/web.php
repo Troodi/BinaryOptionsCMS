878c671daa2fc23b2cef06d5ae58fc78161bdaa4
Troodi <troodi@bk.ru>
2020-08-02 22:01:29 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+use App\Http\Controllers\LanguageController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// dashboard Routes
+Route::get('/','StarterKitController@index');
+
+// locale Route
+Route::get('lang/{locale}',[LanguageController::class,'swap']);
+
+// acess controller
+Route::get('/access-control', 'AccessController@index');
+Route::get('/access-control/{roles}', 'AccessController@roles');
+Route::get('/ecommerce', 'AccessController@home')->middleware('role:Admin');
+
+Auth::routes();
