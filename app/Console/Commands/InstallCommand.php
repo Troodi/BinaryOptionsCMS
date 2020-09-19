7b2a2fc3a6eef91c0b7b65d7973f0cb01310a3e9
Troodi <troodi@bk.ru>
2020-09-19 16:30:24 +0300

    Добавил маркет статус, тестовые валюты

--- app/Console/Commands/InstallCommand.php
+++ app/Console/Commands/InstallCommand.php
@@ -38,21 +38,71 @@ class InstallCommand extends Command
      */
     public function handle()
     {
-        $model = new Symbol();
-        $model->symbol = 'EUR/USD';
-        $model->type = 1;
-        $model->broker = 'FX';
-        $model->percent = 75;
-        $model->status = 1;
-        $model->save();
+        Symbol::truncate();
 
-        $model = new Symbol();
-        $model->symbol = 'BTC/USDT';
-        $model->type = 1;
-        $model->broker = 'BINANCE';
-        $model->percent = 75;
-        $model->status = 1;
-        $model->save();
+        $symbols = [
+          ['BINANCE:BTC/BUSD', mt_rand(50, 85)],
+          ['BINANCE:BTC/IDRT', mt_rand(50, 85)],
+          ['BINANCE:BTC/RUB', mt_rand(50, 85)],
+          ['BINANCE:BTC/ZAR', mt_rand(50, 85)],
+          ['BINANCE:BTC/BKRW', mt_rand(50, 85)],
+          ['BINANCE:BTC/UAH', mt_rand(50, 85)],
+          ['BINANCE:BTC/GBP', mt_rand(50, 85)],
+          ['BINANCE:BTC/NGN', mt_rand(50, 85)],
+          ['BINANCE:BTC/USDC', mt_rand(50, 85)],
+          ['BINANCE:BTC/DAI', mt_rand(50, 85)],
+          ['BINANCE:BTC/USDT', mt_rand(50, 85)],
+          ['BINANCE:BTC/AUD', mt_rand(50, 85)],
+          ['BINANCE:BTC/PAX', mt_rand(50, 85)],
+          ['BINANCE:BTC/TUSD', mt_rand(50, 85)],
+          ['BINANCE:BTC/BIDR', mt_rand(50, 85)],
+          ['BINANCE:BTC/EUR', mt_rand(50, 85)],
+          ['BINANCE:BTC/TRY', mt_rand(50, 85)],
+          ['BINANCE:BTC/USDTPERP', mt_rand(50, 85)],
+          ['BITFINEX:BTC/XCH', mt_rand(50, 85)],
+          ['BITFINEX:BTC/UST', mt_rand(50, 85)],
+          ['BITFINEX:BTC/EUR', mt_rand(50, 85)],
+          ['BITFINEX:BTC/JPY', mt_rand(50, 85)],
+          ['BITFINEX:BTC/USD', mt_rand(50, 85)],
+          ['BITFINEX:BTC/GBP', mt_rand(50, 85)],
+          ['BITFINEX:BTC/CNHT', mt_rand(50, 85)],
+          ['BITFLYER:BTC/JPY3M', mt_rand(50, 85)],
+          ['BITFLYER:BTC/JPY', mt_rand(50, 85)],
+          ['BITHUMB:BTC/KRW', mt_rand(50, 85)],
+          ['BITSO:BTC/MXN', mt_rand(50, 85)],
+          ['BITSTAMP:BTC/EUR', mt_rand(50, 85)],
+          ['BITSTAMP:BTC/USD', mt_rand(50, 85)],
+          ['BITSTAMP:BTC/GBP', mt_rand(50, 85)],
+          ['BITSTAMP:BTC/PAX', mt_rand(50, 85)],
+          ['BINANE:ETH/USDT', mt_rand(50, 85)],
+          ['BITTREX:BTC/USD', mt_rand(50, 85)],
+          ['BITTREX:BTC/EUR', mt_rand(50, 85)],
+          ['FX_IDC:EUR/USD', mt_rand(50, 85)],
+          ['FX_IDC:GBP/USD', mt_rand(50, 85)],
+          ['FX_IDC:USD/CHF', mt_rand(50, 85)],
+          ['FX_IDC:EUR/GBP', mt_rand(50, 85)],
+          ['FX_IDC:EUR/JPY', mt_rand(50, 85)],
+          ['FX_IDC:USD/SEK', mt_rand(50, 85)],
+          ['FX_IDC:USD/RUB', mt_rand(50, 85)],
+          ['FX_IDC:USD/NOK', mt_rand(50, 85)],
+          ['FX_IDC:EUR/CHF', mt_rand(50, 85)],
+          ['FX_IDC:EUR/SEK', mt_rand(50, 85)],
+          ['FX_IDC:EUR/NOK', mt_rand(50, 85)],
+          ['FX_IDC:EUR/AUD', mt_rand(50, 85)],
+          ['FX_IDC:CAD/CHF', mt_rand(50, 85)],
+          ['FX_IDC:CHF/RUB', mt_rand(50, 85)],
+        ];
+
+        foreach ($symbols as $symbol) {
+          $exploded = explode(':', $symbol[0]);
+          $model = new Symbol();
+          $model->symbol = $exploded[1];
+          $model->type = 1;
+          $model->broker = $exploded[0];
+          $model->percent = $symbol[1];
+          $model->status = 1;
+          $model->save();
+        }
 
         return 0;
     }
