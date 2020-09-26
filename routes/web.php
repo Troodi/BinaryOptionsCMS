a895638e38fd1f7bf74c64ab9e3ff4974e8bdecf
Troodi <troodi@bk.ru>
2020-09-26 13:20:50 +0300

    Добаил файл для звонка

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 Auth::routes();
 
 Route::post('/event-twilio', 'Main\ProfileController@phoneEvent');
+Route::get('/mp3', 'Main\ProfileController@playMP3');
 Route::middleware(['auth'])->group(function () {
   // Трейдинг
   Route::post("/binary/buy", 'Main\TradingController@buySymbol');
