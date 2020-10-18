6c075e54840fa91400da530fe2567d1b775e905c
Troodi <troodi@bk.ru>
2020-10-18 20:56:50 +0300

    Обновил модель

--- resources/vuejs/views/user/Deposit.vue
+++ resources/vuejs/views/user/Deposit.vue
@@ -214,13 +214,19 @@
             },
             proccess: function () {
               let self = this;
+              this.errors = [];
+              this.success = [];
               axios.post('/data/deposit', { amount: self.numericAmount, promocode: self.selected_promocode})
                   .then(function (response) {
-                    toastr.success(response.data.message, 'Успешно!', {
-                      positionClass: 'toast-bottom-left',
-                      containerId: 'toast-bottom-left'
-                    });
-                    setTimeout(() => { window.open(response.data.link,'_blank'); }, response.data.timeout);
+                    if(response.data.success === true) {
+                      toastr.success(response.data.message, 'Успешно!', {
+                        positionClass: 'toast-bottom-left',
+                        containerId: 'toast-bottom-left'
+                      });
+                      setTimeout(() => { window.open(response.data.link,'_blank'); }, response.data.timeout);
+                    } else {
+                      self.errors.push(response.data.message);
+                    }
                   })
             }
         },
