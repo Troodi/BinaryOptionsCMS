e201c2bf52f81c35b7942bd721fddae4a5a5f715
Troodi <troodi@bk.ru>
2020-11-01 00:55:17 +0300

    Почтовый шаблон для восстановления пароля

--- app/User.php
+++ app/User.php
@@ -5,12 +5,31 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable, HasApiTokens;
+
+    public function sendPasswordResetNotification($token){
+      $mail_data = [
+        'headline' => 'Восстановление пароля',
+        'subtitle' =>  'Запрошено восстановление пароля от вашего аккаунта',
+        'text' => '<p>Hello! You are receiving this email because we received a password reset request for your account. This password reset link will expire in 60 minutes.</p>',
+        'image' => 'user-reset-password.png',
+        'button_link' => route('password.reset', ['token' => $token, 'email' => $this->email]),
+        'button_text' => 'Reset Password'
+      ];
+      Mail::send('mail.mail', $mail_data, function($message)
+      {
+        $message->from(env('MAIL_USERNAME'), env('APP_NAME'));
+        $message->replyTo(env('MAIL_USERNAME'));
+        $message->subject('Восстановление пароля');
+        $message->to($this->email);
+      });
+    }
 
     /**
      * The attributes that are mass assignable.
