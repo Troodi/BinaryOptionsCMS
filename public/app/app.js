71c23505a1d681041f19b154435ae53470425f5c
Troodi <troodi@bk.ru>
2020-11-05 01:45:50 +0300

    Закрытие рынка на кнопках

--- public/app/app.js
+++ public/app/app.js
@@ -4933,6 +4933,14 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "Trading",
@@ -126008,8 +126016,36 @@ var render = function() {
                           {
                             name: "show",
                             rawName: "v-show",
-                            value: _vm.percent !== "",
-                            expression: "percent !== ''"
+                            value:
+                              isNaN(parseInt(_vm.percent)) &&
+                              _vm.percent !== "",
+                            expression:
+                              "isNaN(parseInt(percent)) && percent !== ''"
+                          }
+                        ],
+                        staticClass:
+                          "disabled btn btn-success mb-1 w-100 btn-lg",
+                        attrs: { type: "button", disabled: "disabled" }
+                      },
+                      [
+                        _vm._v(
+                          "\n                                      Рынок закрыт\n                                    "
+                        )
+                      ]
+                    ),
+                    _vm._v(" "),
+                    _c(
+                      "button",
+                      {
+                        directives: [
+                          {
+                            name: "show",
+                            rawName: "v-show",
+                            value:
+                              _vm.percent !== "" &&
+                              !isNaN(parseInt(_vm.percent)),
+                            expression:
+                              "percent !== '' && !isNaN(parseInt(percent))"
                           }
                         ],
                         staticClass: "btn btn-success glow w-100 btn-lg",
@@ -126061,8 +126097,36 @@ var render = function() {
                           {
                             name: "show",
                             rawName: "v-show",
-                            value: _vm.percent !== "",
-                            expression: "percent !== ''"
+                            value:
+                              isNaN(parseInt(_vm.percent)) &&
+                              _vm.percent !== "",
+                            expression:
+                              "isNaN(parseInt(percent)) && percent !== ''"
+                          }
+                        ],
+                        staticClass:
+                          "disabled btn btn-danger mb-1 w-100 btn-lg",
+                        attrs: { type: "button", disabled: "disabled" }
+                      },
+                      [
+                        _vm._v(
+                          "\n                                      Рынок закрыт\n                                    "
+                        )
+                      ]
+                    ),
+                    _vm._v(" "),
+                    _c(
+                      "button",
+                      {
+                        directives: [
+                          {
+                            name: "show",
+                            rawName: "v-show",
+                            value:
+                              _vm.percent !== "" &&
+                              !isNaN(parseInt(_vm.percent)),
+                            expression:
+                              "percent !== '' && !isNaN(parseInt(percent))"
                           }
                         ],
                         staticClass: "btn btn-danger glow w-100 mt-1 btn-lg",
