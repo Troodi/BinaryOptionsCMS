d7c65025253945745eb7ff6c951a7753ee371c02
Troodi <troodi@bk.ru>
2020-12-01 21:54:13 +0300

    Основные оповещения по почте

--- app/Http/Controllers/Admin/VerifyController.php
+++ app/Http/Controllers/Admin/VerifyController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Models\File;
 use App\Models\Models\VerifyRequest;
 use App\Models\Profile;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Yajra\DataTables\DataTables;
 
 class VerifyController extends Controller
@@ -26,7 +28,9 @@ class VerifyController extends Controller
     ]);
     $files = ['1' => 'document_first_page', '2' => 'document_second_page', '3' => 'document_additional'];
     $pages = ['1' => 'document_first_page_verify_at', '2' => 'document_second_page_verify_at', '3' => 'document_additional_verify_at'];
+    $success = false;
     if($request->status == 1){
+      $success = true;
       Profile::where('user_id', $request->id)->update([$pages[$request->page] => Carbon::now()]);
     } elseif($request->status == 0){
       Profile::where('user_id', $request->id)->update([$files[$request->page] => null, $pages[$request->page] => null]);
@@ -38,7 +42,21 @@ class VerifyController extends Controller
       Profile::where('user_id', $request->id)->update([$files[$request->page] => null, $pages[$request->page] => null]);
     }
     VerifyRequest::where('user_id', $request->id)->where('page', $request->page)->delete();
-    //TODO Добавить сообщение о проверки документа пользователю
+    $mail_data = [
+      'headline' => $success ? "Документ №$request->page прошел проверку" : "Документ №$request->page не прошел проверку",
+      'subtitle' =>  'Статус проверки документа',
+      'text' => $success ? '<p>Поздравляем! Ваш документ был проверен и подтвержден</p>' : '<p>К сожалению, документ не прошел проверку</p>',
+      'image' => $success ? 'membership.png' : 'membership-ended.png',
+      'button_link' => env('APP_URL').'/profile',
+      'button_text' => 'Перейти в кабинет'
+    ];
+    Mail::send('mail.mail', $mail_data, function($message) use ($request, $success)
+    {
+      $message->from(env('MAIL_USERNAME'), env('APP_NAME'));
+      $message->replyTo(env('MAIL_USERNAME'));
+      $message->subject($success ? "Документ №$request->page прошел проверку" : "Документ №$request->page не прошел проверку");
+      $message->to(User::where('id', $request->id)->first()->email);
+    });
     return response()->json(['success' => true, 'message' => 'Действие выполнено успешно!']);
   }
 
@@ -49,7 +67,21 @@ class VerifyController extends Controller
     ]);
     Profile::where('user_id', $request->id)->update(['user_verify_at' => Carbon::now()]);
     VerifyRequest::where('user_id', $request->id)->delete();
-    //TODO Добавить сообщение о верификации пользователю
+    $mail_data = [
+      'headline' => "Аккаунт верифицирован",
+      'subtitle' =>  'Теперь у вас подтвержденный аккаунт',
+      'text' => '<p>Поздравляем! Ваши данные были проверены и успешно подтверждены.</p>',
+      'image' => 'subscription-completed.png',
+      'button_link' => env('APP_URL').'/profile',
+      'button_text' => 'Перейти в кабинет'
+    ];
+    Mail::send('mail.mail', $mail_data, function($message) use ($request)
+    {
+      $message->from(env('MAIL_USERNAME'), env('APP_NAME'));
+      $message->replyTo(env('MAIL_USERNAME'));
+      $message->subject("Аккаунт верифицирован");
+      $message->to(User::where('id', $request->id)->first()->email);
+    });
     return response()->json(['success' => true, 'message' => 'Пользователь успешно верифицирован!']);
   }
 }
--- app/Http/Controllers/Admin/WithdrawalController.php
+++ app/Http/Controllers/Admin/WithdrawalController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Ixudra\Curl\Facades\Curl;
 
 class WithdrawalController extends Controller
@@ -24,54 +25,88 @@ class WithdrawalController extends Controller
       }
       if($request->status == 2){
         User::where('id', $withdrawal->user_id)->update(['balance' => DB::raw("balance+$withdrawal->amount")]);
+        Withdrawal::where('id', $request->id)->update(['status' => $request->status, 'message' => $request->comment]);
+        $message = $request->comment ? 'Причина: '.$request->comment : 'Причина отклонения не была указана';
+        $mail_data = [
+          'headline' => "Выплата отклонена",
+          'subtitle' =>  'Вывод средств не был осуществлен',
+          'text' => "<p>К сожалению, выплата не произведена. $message</p>",
+          'image' => 'order-cancel.png',
+          'button_link' => env('APP_URL').'/profile',
+          'button_text' => 'Перейти в кабинет'
+        ];
+        Mail::send('mail.mail', $mail_data, function($message) use ($request, $withdrawal)
+        {
+          $message->from(env('MAIL_USERNAME'), env('APP_NAME'));
+          $message->replyTo(env('MAIL_USERNAME'));
+          $message->subject("Выплата отклонена");
+          $message->to(User::where('id', $withdrawal->user_id)->first()->email);
+        });
+        return response()->json(['success' => true, 'message' => 'Заявка на выплату была отклонена!']);
       }
-      // Check balance
-      $response = Curl::to('https://payeer.com/ajax/api/api.php?checkUser')
-        ->withData([
-          'apiId' => env('PAYEER_API_PAYOUT_ID'),
-          'apiPass' => env('PAYEER_API_PAYOUT_SECRET'),
-          'account' => env('PAYEER_API_WALLET'),
-          'action' => 'getBalance',
-        ])
-        ->asJsonResponse()
-        ->post();
-      if($withdrawal->amount > $response->balance->USD->available){
-        return response()->json(['success' => false, 'message' => 'В платежной системе недостаточно средств для выплаты!']);
-      }
-      //Check user wallet exist
-      $response = Curl::to('https://payeer.com/ajax/api/api.php?checkUser')
-        ->withData([
-          'apiId' => env('PAYEER_API_PAYOUT_ID'),
-          'apiPass' => env('PAYEER_API_PAYOUT_SECRET'),
-          'account' => env('PAYEER_API_WALLET'),
-          'action' => 'checkUser',
-          'user' => $withdrawal->address
-        ])
-        ->asJsonResponse()
-        ->post();
-        if($response->errors){
+      if($request->status == 1) {
+        // Check balance
+        $response = Curl::to('https://payeer.com/ajax/api/api.php?checkUser')
+          ->withData([
+            'apiId' => env('PAYEER_API_PAYOUT_ID'),
+            'apiPass' => env('PAYEER_API_PAYOUT_SECRET'),
+            'account' => env('PAYEER_API_WALLET'),
+            'action' => 'getBalance',
+          ])
+          ->asJsonResponse()
+          ->post();
+        if ($withdrawal->amount > $response->balance->USD->available) {
+          return response()->json(['success' => false, 'message' => 'В платежной системе недостаточно средств для выплаты!']);
+        }
+        //Check user wallet exist
+        $response = Curl::to('https://payeer.com/ajax/api/api.php?checkUser')
+          ->withData([
+            'apiId' => env('PAYEER_API_PAYOUT_ID'),
+            'apiPass' => env('PAYEER_API_PAYOUT_SECRET'),
+            'account' => env('PAYEER_API_WALLET'),
+            'action' => 'checkUser',
+            'user' => $withdrawal->address
+          ])
+          ->asJsonResponse()
+          ->post();
+        if ($response->errors) {
           return response()->json(['success' => false, 'message' => 'Не существует такого кошелька в платежной системе!']);
         }
-      // Send money
-      $response = Curl::to('https://payeer.com/ajax/api/api.php?transfer')
-        ->withData([
-          'apiId' => env('PAYEER_API_PAYOUT_ID'),
-          'apiPass' => env('PAYEER_API_PAYOUT_SECRET'),
-          'account' => env('PAYEER_API_WALLET'),
-          'action' => 'transfer',
-          'curIn' => 'USD',
-          'sum' => $withdrawal->amount,
-          'curOut' => 'USD',
-          'to' => $withdrawal->address
-        ])
-        ->asJsonResponse()
-        ->post();
-      if($response->errors){
-        Log::info($response);
-        return response()->json(['success' => false, 'message' => 'Не удалось отправить средства!']);
+        // Send money
+        $response = Curl::to('https://payeer.com/ajax/api/api.php?transfer')
+          ->withData([
+            'apiId' => env('PAYEER_API_PAYOUT_ID'),
+            'apiPass' => env('PAYEER_API_PAYOUT_SECRET'),
+            'account' => env('PAYEER_API_WALLET'),
+            'action' => 'transfer',
+            'curIn' => 'USD',
+            'sum' => $withdrawal->amount,
+            'curOut' => 'USD',
+            'to' => $withdrawal->address
+          ])
+          ->asJsonResponse()
+          ->post();
+        if ($response->errors) {
+          Log::info(serialize($response));
+          return response()->json(['success' => false, 'message' => 'Не удалось отправить средства!']);
+        }
+        //
+        Withdrawal::where('id', $request->id)->update(['status' => $request->status, 'message' => $request->comment]);
+        $mail_data = [
+          'headline' => "Средства успешно выплачены",
+          'subtitle' => 'Выплата произведена',
+          'text' => '<p>Поздравляем! Вам была произведена выплата.</p>',
+          'image' => 'order-refund.png',
+          'button_link' => env('APP_URL') . '/profile',
+          'button_text' => 'Перейти в кабинет'
+        ];
+        Mail::send('mail.mail', $mail_data, function ($message) use ($request, $withdrawal) {
+          $message->from(env('MAIL_USERNAME'), env('APP_NAME'));
+          $message->replyTo(env('MAIL_USERNAME'));
+          $message->subject("Средства успешно выплачены");
+          $message->to(User::where('id', $withdrawal->user_id)->first()->email);
+        });
       }
-      //
-      Withdrawal::where('id', $request->id)->update(['status' => $request->status, 'message' => $request->comment]);
       return response()->json(['success' => true, 'message' => 'Заявка успешно обработана!']);
     }
 }
