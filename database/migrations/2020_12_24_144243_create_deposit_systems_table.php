2f2bfa60afd617883251617f5b973f9193ac842a
Troodi <troodi@bk.ru>
2020-12-28 23:05:32 +0300

    Сортировка для методов пополнения

--- database/migrations/2020_12_24_144243_create_deposit_systems_table.php
+++ database/migrations/2020_12_24_144243_create_deposit_systems_table.php
@@ -18,10 +18,11 @@ class CreateDepositSystemsTable extends Migration
             $table->id();
             $table->string('text');
             $table->boolean('hidden')->default(false);
+            $table->boolean('order')->nullable();
             $table->timestamps();
         });
 
-        $systems = ['Cards (Qiwi)' => 0, 'YooMoney' => 1, 'Betatransfer' => 1, 'Payeer' => 1, 'Free-Kassa' => 0, 'InterKassa' => 1, 'RosKassa' => 1, 'Robokassa' => 1];
+        $systems = ['Cards (Qiwi)' => 0, 'YooMoney' => 0, 'Betatransfer' => 1, 'Payeer' => 1, 'Free-Kassa' => 0, 'InterKassa' => 1, 'RosKassa' => 1, 'Robokassa' => 1];
 
         foreach ($systems as $system => $hidden) {
           $model = new DepositSystem();
@@ -29,6 +30,9 @@ class CreateDepositSystemsTable extends Migration
           $model->hidden = $hidden;
           $model->save();
         }
+        DepositSystem::where('id', 2)->update(['order', 1]);
+        DepositSystem::where('id', 1)->update(['order', 2]);
+        DepositSystem::where('id', 5)->update(['order', 3]);
     }
 
     /**
