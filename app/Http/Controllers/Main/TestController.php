c38f2d4e628875525101c72c732c9284eaa18489
Troodi <troodi@bk.ru>
2020-12-28 21:10:26 +0300

    YooMoney

--- app/Http/Controllers/Main/TestController.php
+++ app/Http/Controllers/Main/TestController.php
@@ -6,11 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Models\Symbols\Options\Ticks;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Ixudra\Curl\Facades\Curl;
 use Qiwi\Api\BillPayments;
 
 class TestController extends Controller
 {
   public function test(Request $request){
-    dump(geoip('95.221.53.80')['iso_code']);
+    $orderId = 123;
+    $amount = 1;
+    $request = Curl::to("https://yoomoney.ru/quickpay/confirm.xml?receiver=41001828768205&quickpay-form=shop&targets=Deposit&sum=$amount&label=$orderId")
+      ->get();
+    $link = str_replace('Found. Redirecting to ', '', $request);
   }
 }
