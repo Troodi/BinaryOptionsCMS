a2a423c94dbc92ab0e08cc697a8fd629db228bc8
Troodi <troodi@bk.ru>
2021-01-01 20:06:22 +0300

    Временно битки основной способ

--- public/app/app.js
+++ public/app/app.js
@@ -2087,8 +2087,8 @@ window.dataLoaded = false;
         symbol = localStorage.getItem('symbol_full');
         tabSymbol = localStorage.getItem('symbol_short');
       } else {
-        symbol = 'Binary:EUR/USD';
-        tabSymbol = 'EUR/USD';
+        symbol = 'Binary:BTC/USDT';
+        tabSymbol = 'BTC/USDT';
       }
 
       if (localStorage.getItem('resolution')) {
