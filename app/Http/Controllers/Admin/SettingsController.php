9c92bba671fac7b23fba872ab869d4970a2fb6e9
Troodi <troodi@bk.ru>
2021-01-30 13:49:25 +0300

    Обновил настройки

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -12,4 +12,15 @@ class SettingsController extends Controller
     $keys = DotenvEditor::getKeys();
     return $keys;
   }
+
+  public function save(Request $request){
+    DotenvEditor::autoBackup(false);
+    $result = null;
+    parse_str($request->data, $result);
+    foreach ($result as $key => $value) {
+      DotenvEditor::setKey($key, $value, '', false);
+    }
+    DotenvEditor::save();
+    return response()->json(['success' => true, 'message' => 'Настройки успешно сохранены!']);
+  }
 }
