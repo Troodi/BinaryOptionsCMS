d9d6be982c47baa46579d684a75e7f0431531165
Troodi <troodi@bk.ru>
2021-02-01 00:58:43 +0300

    Редактирование валютных пар

--- app/Http/Controllers/Admin/SymbolController.php
+++ app/Http/Controllers/Admin/SymbolController.php
@@ -11,4 +11,68 @@ class SymbolController extends Controller
   public function allSymbols(Request $request){
     return Symbol::orderBy('id', 'asc')->get();
   }
+
+  public function saveSymbol(Request $request){
+    $request->validate([
+      'id' => 'required|numeric|min:1',
+      'pair' => 'required|string|min:1',
+      'broker' => 'required|string|min:1',
+      'fix' => 'required|numeric|min:1|max:100',
+      'min' => 'required|numeric|min:1|max:100',
+      'max' => 'required|numeric|min:1|max:100',
+      'work_from' => 'required|numeric|min:0|max:23',
+      'work_to' => 'required|numeric|min:0|max:23',
+      'active' => 'required|numeric|min:0|max:1',
+    ]);
+    Symbol::where('id', $request->id)->update([
+      'symbol' => $request->pair,
+      'broker' => $request->broker,
+      'fixed_percent' => $request->fix,
+      'min_percent' => $request->min,
+      'max_percent' => $request->max,
+      'work_from' => $request->work_from,
+      'work_to' => $request->work_to,
+      'status' => $request->active,
+    ]);
+    return response()->json(['success' => true, 'message' => 'Данные успешно обновлены!']);
+  }
+
+  public function getSymbol(Request $request){
+    $request->validate([
+      'id' => 'required|integer|min:1'
+    ]);
+    return Symbol::where('id', $request->id)->first();
+  }
+
+  public function removeSymbol(Request $request){
+    $request->validate([
+      'id' => 'required|integer|min:1'
+    ]);
+    Symbol::where('id', $request->id)->delete();
+    return response()->json(['success' => true, 'message' => 'Символ успешно удален!']);
+  }
+
+  public function createSymbol(Request $request){
+    $request->validate([
+      'pair' => 'required|string|min:1',
+      'broker' => 'required|string|min:1',
+      'fix' => 'required|numeric|min:1|max:100',
+      'min' => 'required|numeric|min:1|max:100',
+      'max' => 'required|numeric|min:1|max:100',
+      'work_from' => 'required|numeric|min:0|max:23',
+      'work_to' => 'required|numeric|min:0|max:23',
+      'status' => 'required|numeric|min:0|max:1',
+    ]);
+    $model = new Symbol();
+    $model->symbol = $request->pair;
+    $model->broker = $request->broker;
+    $model->fixed_percent = $request->fix;
+    $model->min_percent = $request->min;
+    $model->max_percent = $request->max;
+    $model->work_from = $request->work_from;
+    $model->work_to = $request->work_to;
+    $model->status = $request->status;
+    $model->save();
+    return response()->json(['success' => true, 'message' => 'Символ успешно создан!', 'created' => $model]);
+  }
 }
