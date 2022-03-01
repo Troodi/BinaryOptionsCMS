4cd3a3a6e6e1be460070dc5a594a9d8a873603ca
Troodi <troodi@ya.ru>
2022-03-01 19:23:11 +0300

    Fixed package json

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,11 @@ echo "Entry point started!"
 php-fpm &
 echo "PHP-FPM started!"
 if [ ! -d /var/www/node_modules ]; then
+  (
+    apt-get purge -y nodejs
+  )
+  curl -sL https://deb.nodesource.com/setup_14.x | bash -
+  apt-get install -y nodejs
   echo "Install composer and node_modules with npm compiling!"
   composer install
   npm install
