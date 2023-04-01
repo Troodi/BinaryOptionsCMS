241c9d20bd6a5795cdfb47f1fb9e8b9f344aee04
VadimKutsyi <kutsyy.vadimka@gmail.com>
2023-04-01 20:26:34 +0300

    Second commit

--- app/Console/Commands/SetSymbolsPercent.php
+++ app/Console/Commands/SetSymbolsPercent.php
@@ -7,6 +7,7 @@ use App\Models\Symbols\Options\Symbol;
 use App\Models\SymbolShortStatistic;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use App\Services\SetSymbolsPercentService;
 
 class SetSymbolsPercent extends Command
 {
@@ -39,35 +40,8 @@ class SetSymbolsPercent extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(SetSymbolsPercentService $setSymbolsPercentService)
     {
-      foreach(Symbol::all() as $symbol){
-        $stat = SymbolShortStatistic::where('symbol_id', $symbol->id)->get();
-        $count = $stat->count();
-        if($count){
-          $id = SymbolShortStatistic::where('symbol_id', $symbol->id)->latest()->first()->id;
-          SymbolShortStatistic::where('symbol_id', $symbol->id)
-            ->where('created_at', '<', Carbon::now()->subMinutes(30))
-            ->where('id', '<', $id)
-            ->delete();
-        }
-        if($count >= 10){
-          $profit_count = $stat->filter(function ($value, $key) {
-            return $value->profit > $value->amount;
-          })->count();
-          $percent = intval(100 - ($profit_count / $count * 100)); // 130 т.к. на 30 процентов будет повышена доходность, чтобы не была слишком низкой
-          if($percent < $symbol->min_percent){
-            $percent = $symbol->min_percent;
-          }
-          elseif($percent > $symbol->max_percent){
-            $percent = $symbol->max_percent;
-          }
-          Symbol::where('id', $symbol->id)->update(['percent' => $percent]);
-        } else {
-          Symbol::where('id', $symbol->id)->update(['percent' => $symbol->fixed_percent]);
-        }
-      }
-      broadcast(new ChangeSymbol(Symbol::all()));
-      return 0;
+        $setSymbolsPercentService->calculatePercent();
     }
 }
