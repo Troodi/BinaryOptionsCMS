4555cfc9339128269f220dab04c2bf87f947eb3d
troodi <r.larin@stellarlabs.ai>
2023-04-01 20:26:19 +0300

    Docker production

--- env-ci/scripts/let-cert.sh
+++ env-ci/scripts/let-cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CF_Key=
 export CF_Email=
-./acme.sh --register-account -m info@binaries.site
-./acme.sh --issue --dns dns_cf -d binaries.site -d *.binaries.site --cert-file $PWD'/certs/domain-cert.pem' --key-file $PWD'/certs/domain-key.key' --ca-file $PWD'/certs/domain-ca.pem' --fullchain-file $PWD'/certs/domain-fullchain.pem' --force >> $PWD/letencrypt/renew.sh
+./acme.sh --register-account -m info@binaries.website
+./acme.sh --issue --dns dns_cf -d binaries.website -d *.binaries.website --cert-file $PWD'/../docker/nginx/certs/domain-cert.pem' --key-file $PWD'/docker/nginx/certs/domain.key' --ca-file $PWD'/docker/nginx/certs/domain-ca.pem' --fullchain-file $PWD'/docker/nginx/certs/domain.crt' --force >> $PWD/letencrypt/renew.sh
